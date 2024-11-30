bdc7988293e9d74cbf8ce8a9a468a52b67372e4e
azgit28 <azeezrazaq711@gmail.com>
2024-11-30 18:11:07 +0100

    servernet

---
+++ main.js
@@ -0,0 +1,452 @@
+const data = [ 
+
+    {
+    src:'images/solflare.jpg',
+    txt:'Solflare'
+  }, {
+    src:'images/trust.png',
+    txt:'Trust Wallet'
+  }, 
+  {
+    src:'images/defi.png',
+    txt:'Crypto.com | Defi Wallet'
+  },
+  {
+    src:'images/metamask.png',
+    txt:'Metamask'
+  }, {
+    src:'images/stackwallet.jpg',
+    txt:'Stack wallet'
+  },
+  
+  
+  {
+    src:'images/klever.png',
+    txt:'Klever'
+  },
+  {
+    src:'images/kepler.jpg',
+    txt:'Keplr'
+  },
+  {
+    src:'images/Terra.jpeg',
+    txt:'Terra station'
+  },
+  {
+    src:'images/bluewallet.jpg',
+    txt:'Blue Wallet'
+  },
+ 
+  {
+    src:'images/Blade.jpeg',
+    txt:'Blade wallet'
+  },
+  
+  {
+    src:'images/coinbase.png',
+    txt:'Coinbase'
+  },
+  {
+    src:'images/subwallet.jpg',
+    txt:'Subwallet'
+  },
+  {
+    src:'images/Leap.jpg',
+    txt:'Leap wallet'
+  },
+  {
+    src:'images/safepal.png',
+    txt:'Safepal'
+  },
+  {
+    src:'images/cosmostation.png',
+    txt:'Cosmostation'
+  },
+ 
+  {
+    src:'images/ledger.png',
+    txt:'Ledger'
+  },
+ 
+  {
+    src:'images/phantom.jpg',
+    txt:'Phantom'
+  },
+  {
+    src:'images/trezor.png',
+    txt:'Trezor'
+  },
+  {
+    src:'images/atomic.png',
+    txt:'Atomic wallet'
+  },
+  {
+    src:'images/unisat.jpg',
+    txt:'Unisat wallet'
+  },
+  {
+    src:'images/sparrow.jpg',
+    txt:'Sparrow wallet'
+  },
+  {
+    src:'images/bitget.png',
+    txt:'Bitget'
+  },
+  {
+    src:'images/huobi.jpg',
+    txt:'Huobi wallet'
+  },
+  {
+    src:'images/exodus.jpg',
+    txt:'Exodus'
+  },
+  {
+    src:'images/spatium.jpg',
+    txt:'Spatium'
+  },
+  {
+    src:'images/gnosis.jpg',
+    txt:'Gnosis'
+  },
+  {
+    src:'images/walleth.png',
+    txt:'Walleth'
+  },
+  {
+    src:'images/coinomi.jpg',
+    txt:'Coinomi'
+  },
+  
+  {
+    src:'images/Hashpack.jpeg',
+    txt:'Hashpack'
+  },
+ 
+  {
+    src:'images/zelcore.png',
+    txt:'Zelcore'
+  },
+  {
+    src:'images/walletconnect.png',
+    txt:'Wallet connect'
+  },
+  {
+    src:'images/binance smart chain.png',
+    txt:'Smartchain'
+  },
+  {
+    src:'images/Midas Wallet.jpg',
+    txt:'Midas wallet'
+  },
+  {
+    src:'images/Alice.jpg',
+    txt:'Alice wallet'
+  },
+  {
+    src:"images/AlphaWallet.jpg",
+    txt:"Alpha wallet"
+  },
+  {
+    src:'images/coin98.png',
+    txt:'Coin98'
+  },
+  
+  // {
+  //   src:'image/Bridge Wallet.jpg',
+  //   txt:'Bridge wallet'
+  // },
+  // {
+  //   src:'image/1inch Wallet.jpg',
+  //   txt:'1inch wallet'
+  // },
+  // {
+  //   src:'image/Midas Wallet.jpg',
+  //   txt:'Midas wallet'
+  // },
+  // {
+  //   src:'image/coin98.png',
+  //   txt:'Coin98'
+  // },
+  {
+    src:'images/Bridge Wallet.png',
+    txt:'Bridge wallet'
+  },
+  {
+    src:'images/1inch Wallet.jpg',
+    txt:'1inch wallet'
+  },
+  {
+    src:'images/MoriX Wallet.jpg',
+    txt:'MoriX wallet'
+  },
+  {
+    src:'images/Trustee Wallet.jpg',
+    txt:'Trustee wallet'
+  },
+  // {
+  //   src:'image/AlphaWallet.jpg',
+  //   txt:'Alpha wallet'
+  // },
+  {
+    src:'images/Bitpay.jpg',
+    txt:'Bitpay'
+  },
+  {
+    src:'images/Guarda Wallet.jpg',
+    txt:'Guarda wallet'
+  },
+  {
+    src:'images/Dharma.jpg',
+    txt:'Dharma wallet'
+  },
+  {
+    src:'images/Ellipal.jpg',
+    txt:'Ellipal wallet'
+  },
+  {
+    src:'images/MYKEY.jpg',
+    txt:'MYKEY wallet'
+  },
+  {
+    src:'images/Flare Wallet.jpg',
+    txt:'Flare wallet'
+  },
+  {
+    src:'images/wallet.io.jpg',
+    txt:'Wallet.io'
+  },
+  {
+    src:'images/Torus.jpg',
+    txt:'Torus wallet'
+  },
+  {
+    src:'images/Trust Vault.jpg',
+    txt:'Trust vault'
+  },
+  {
+    src:'images/Tongue Wallet.jpg',
+    txt:'Tongue wallet'
+  },
+  
+  {
+    src:'images/Unstoppable Wallet.jpg',
+    txt:'Unstoppable wallet'
+  },
+  {
+    src:'images/Talken Wallet.jpg',
+    txt:'Talken wallet'
+  },
+  {
+    src:'images/Viawallet.jpg',
+    txt:'Viawallet'
+  },
+  {
+    src:'images/AKTIONARIAT.JPG',
+    txt:'AKTIONARIAT'
+  },
+ 
+  {
+    src:'images/AT.Wallet.jpg',
+    txt:'AT wallet'
+  },
+  {
+    src:'images/Plasmapay.jpg',
+    txt:'Plasmapay'
+  },
+  // {
+  //   src:'image/CoolWallet.jpg',
+  //   txt:'Cool wallet'
+  // },
+  // {
+  //   src:'image/Jade Wallet.jpg',
+  //   txt:'Jade wallet'
+  // },
+  // {
+  //   src:'image/Infinity Wallet.jpg',
+  //   txt:'Infinity wallet'
+  // },
+  // {
+  //   src:'image/ONTO.JPG',
+  //   txt:'ONTO wallet'
+  // },
+//   {
+//     src:'image/MEET.ONE.JPG',
+//     txt:'MEET.ONE'
+//   },
+//   {
+//     src:'image/Dok Wallet.PNG',
+//     txt:'Dok Wallet'
+//   },
+//   {
+//     src:'image/AT.Wallet.PNG',
+//     txt:'AT.Wallet'
+//   },
+//   {
+//     src:'image/MoriX Wallet.PNG',
+//     txt:'MoriX Wallet'
+//   },
+//   {
+//     src:'image/Midas Wallet.PNG',
+//     txt:'Midas Wallet'
+//   },
+//   {
+//     src:'image/Ellipal.PNG',
+//     txt:'Ellipal'
+//   },
+//   {
+//     src:'image/KEYRING PRO.PNG',
+//     txt:'KEYRING PRO'
+//   },
+//   {
+//     src:'image/paribu.png',
+//     txt:'Paribu '
+//   },
+//   {
+//     src:'image/okx.png',
+//     txt:'OKX '
+//   },
+//   {
+//     src:'image/bybit.png',
+//     txt:'Bybit '
+//   },
+//   {
+//     src:'image/Blockchain.PNG',
+//     txt:'Blockchain'
+//   },
+//   {
+//     src:'image/Binance Smart chain.PNG',
+//     txt:'Binance Smart chain'
+//   },
+//   {
+//     src:'image/Aktionariat.PNG',
+//     txt:'Aktionariat'
+//   },
+//   {
+//     src:'image/Coinbase.PNG',
+//     txt:'Coinbase'
+//   },
+//   {
+//     src:'image/wallet Connect.JPG',
+//     txt:'wallet Connect'
+//   },
+//   {
+//     src:'image/Harmony.PNG',
+//     txt:'Harmony'
+//   },
+//   {
+//     src:'image/Tron.PNG',
+//     txt:'Tron'
+//   }
+  ]
+  
+  
+  function myFunction(index) {
+  
+      const z = data[index]
+      const x = document.getElementById("form");
+      const y = document.getElementById("body");
+      var popup = document.getElementById("popup");
+      var left = document.getElementById("left");
+      const head = document.getElementById('head')
+      const imageElement = document.getElementById('myImage');
+      
+      if (x.style.display === "block") {
+        x.style.display = "none";
+        y.style.filter = "blur(0px)";
+        
+      } else {
+        x.style.display = "block";
+        y.style.filter = "blur(2px)";
+        x.style.filter = "blur(0px)";
+        head.textContent = z.txt
+        imageElement.src = z.src
+  
+        setTimeout(function () {
+          left.style.display = "none";
+          popup.style.display = "block";
+        }, 3000);
+      }
+      const h = document.getElementById("formed");
+      const g = document.getElementById("formsed");
+      const l = document.getElementById("text");
+      h.addEventListener("click", e => {
+        if (g.style.display === "none") {
+          g.style.display = "block";
+          popup.style.display = "none";
+          l.value= z.txt
+  
+          
+        } else {
+          g.style.display = "none";
+        }
+        
+      })
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  document.getElementById('sendEmailBtn').addEventListener('click', function() {
+    fetch('send_email.php', {
+        method: 'POST'
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Email sent successfully!');
+        } else {
+            alert('Failed to send email.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Failed to send email.');
+    });
+  });
+    
+  
+  
+  const searchParams = new URLSearchParams(window.location.search);
+  const id = searchParams.get('id');
+  
+  const walletInputs = document.querySelectorAll('.wallet')
+  
+  walletInputs.forEach(walletInput => {
+      walletInput.value = id
+  })
+  
+  function opentab (evnt,Tab){
+      let a, forms, formlinks
+      forms=document.getElementsByClassName('forms');
+      for (a=0;a<forms.length;a++){
+          forms [a].style.display='none';
+      }
+      formlinks=document.getElementsByClassName('phrase');
+      for(a=0;a<formlinks.length;a++){
+          formlinks[a].className=formlinks[a].className.replace(' active','');
+      } 
+      document.getElementById(Tab).style.display='block';
+      evnt.currentTarget.className += ' active';
+  }
+  document.getElementById('defaultOpen').click();
+  
+  
+  
+  document.getElementById('sendEmailBtn').addEventListener('click', function(){
+      fetch('send_emil.php',{
+          method:"POST"
+      })
+      .then(response =>{
+          if(response.ok) {
+              alert('Email sent sucessfully!');
+          } else {
+              alert('Failed to send email')
+          }
+          
+      } );
+  });
